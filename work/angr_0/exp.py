16bfe2f1f980cf8392aeda101f75e2435d9d53c7
just-xyyy <xieyue0624@163.com>
2022-10-27 22:52:44 +0800

    my first problem solved by angr

---
+++ work/angr_0/exp.py
@@ -0,0 +1,16 @@
+import angr
+# import the program
+p = angr.Project("../../dist/00_angr_find")
+# create a state
+state = p.factory.entry_state()
+# create a simulation execution starts at the parameter
+sm = p.factory.simulation_manager(state)
+# set target branch
+sm.explore(find=0x804867D)
+# the result is stored in the array sm.
+# we can use sm.found[0].posix.dumps(0) get standard input and get standard output by sm.found[0].posix.dumps(1)
+if sm.found[0]:
+    print(sm.found[0].posix.dumps(0))
+    print(sm.found[0].posix.dumps(1))
+# b'JXWVXRKX'
+# b'Enter the password: '
